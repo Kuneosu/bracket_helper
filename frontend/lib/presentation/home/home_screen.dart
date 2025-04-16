fbeadafaeb5559a75e639b73eca8cf5e71b7bbba
Kuneosu <kks92155784@gmail.com>
2025-04-16 12:40:14 +0900

    [BH-12] default_menu_card 구현

--- frontend/lib/presentation/home/home_screen.dart
+++ frontend/lib/presentation/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:bracket_helper/data/database/app_database.dart';
+import 'package:bracket_helper/presentation/home/widget/default_menu_card.dart';
 import 'package:bracket_helper/presentation/home/widget/recent_tournament_card.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,7 @@ class HomeScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text('Home Screen'),
             RecentTournamentCard(
@@ -29,6 +31,13 @@ class HomeScreen extends StatelessWidget {
               onTapDelete: () {
                 debugPrint('onTapDelete');
               },
+            ),
+            SizedBox(height: 20),
+            DefaultMenuCard(
+              title: '대진표\n생성하기',
+              subtitle: '복식/단식 매칭',
+              imagePath: 'assets/image/logo_760.png',
+              onTap: () {},
             ),
           ],
         ),
---
+++ frontend/lib/presentation/home/widget/default_menu_card.dart
@@ -0,0 +1,58 @@
+import 'package:bracket_helper/ui/color_st.dart';
+import 'package:bracket_helper/ui/text_st.dart';
+import 'package:flutter/material.dart';
+
+class DefaultMenuCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String imagePath;
+  final void Function() onTap;
+  const DefaultMenuCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.imagePath,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 335,
+      height: 200,
+      padding: EdgeInsets.all(25),
+      decoration: BoxDecoration(
+        color: CST.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: CST.black.withValues(alpha: 0.1),
+            blurRadius: 6,
+            spreadRadius: 2,
+            offset: Offset(0, 0),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  subtitle,
+                  style: TST.normalTextRegular,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(title, style: TST.headerTextBold),
+              ],
+            ),
+          ),
+          Image.asset(imagePath, width: 120, height: 120),
+        ],
+      ),
+    );
+  }
+}
