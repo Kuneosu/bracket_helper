21fc46e01bb6d34c965f6e39802033503b182874
Kuneosu <kks92155784@gmail.com>
2025-04-16 11:22:55 +0900

    [BH-12] recent_tournament_card 구현

--- frontend/lib/presentation/home/home_screen.dart
+++ frontend/lib/presentation/home/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:bracket_helper/data/database/app_database.dart';
+import 'package:bracket_helper/presentation/home/widget/recent_tournament_card.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +7,32 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Home Screen')));
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Text('Home Screen'),
+            RecentTournamentCard(
+              tournament: Tournament(
+                id: 1,
+                title: 'Tournament A',
+                date: DateTime.now(),
+                winPoint: 1,
+                drawPoint: 0,
+                losePoint: 0,
+                gamesPerPlayer: 1,
+                isDoubles: false,
+              ),
+              onTapCard: () {
+                debugPrint('onTapCard');
+              },
+              onTapDelete: () {
+                debugPrint('onTapDelete');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
