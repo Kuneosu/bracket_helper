e230ee2fe78198e835a82575e48744d78976beef
Kuneosu <kks92155784@gmail.com>
2025-04-29 11:23:35 +0900

    [0.2517.3] Release

--- frontend/ios/ci_scripts/ci_post_clone.sh
+++ frontend/ios/ci_scripts/ci_post_clone.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-set -e  # 오류 발생 시 스크립트 즉시 중단
-
 # 현재 위치는 ios/ci_scripts, 상위로 이동
 cd ../..
 
 # Flutter 패키지 가져오기
-flutter/bin/flutter pub get
+flutter pub get
 
 # iOS 디렉토리로 이동
 cd ios
--- frontend/lib/core/constants/app_strings.dart
+++ frontend/lib/core/constants/app_strings.dart
@@ -1,7 +1,7 @@
 class AppStrings {
 
   static const String appTitle = '대진 도우미';
-  static const String currentVersion = '0.2517.2';
+  static const String currentVersion = '0.2517.3';
 
   // 홈 화면
   static const String help = '도움말';
