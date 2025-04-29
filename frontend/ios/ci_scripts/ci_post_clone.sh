db7b9be55be5c846d8087048a7ca9086e8afa0c8
Kuneosu <kks92155784@gmail.com>
2025-04-29 10:14:40 +0900

    XcodeCloud 빌드용 ci_scripts  수정

--- frontend/ios/ci_scripts/ci_post_clone.sh
+++ frontend/ios/ci_scripts/ci_post_clone.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-brew install cocoapods
+# iOS 폴더로 이동
+cd ios
+
+# CocoaPods 설치 (brew로 설치할 필요 없음, 그냥 pod install만 실행)
 pod install
 
+# 프로젝트 루트로 돌아가기
 cd ..
+
+# Flutter 패키지 설치
 flutter pub get
-
-
