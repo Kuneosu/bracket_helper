293947ed644f37829ed0232fbc1f0ead93dffe88
Kuneosu <kks92155784@gmail.com>
2025-04-29 16:52:08 +0900

    ci_post_clone.sh 수정

--- frontend/ios/ci_scripts/ci_post_clone.sh
+++ frontend/ios/ci_scripts/ci_post_clone.sh
@@ -1,13 +1,18 @@
-#!/bin/bash
+#!/bin/sh
+set -e                           # 오류 발생 시 즉시 종료
 
-# 현재 위치는 ios/ci_scripts, 상위로 이동
-cd ../..
+cd "$CI_PRIMARY_REPOSITORY_PATH" # = 리포지토리 루트
 
-# Flutter 패키지 가져오기
+## 1. Flutter 설치(권장: 버전 고정)
+git clone https://github.com/flutter/flutter.git -b stable --depth 1 $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+## 2. iOS 아티팩트 사전 다운로드
+flutter precache --ios
+
+## 3. 의존성
 flutter pub get
 
-# iOS 디렉토리로 이동
-cd ios
-
-# CocoaPods 설치
-pod install
+## 4. CocoaPods
+HOMEBREW_NO_AUTO_UPDATE=1 brew install cocoapods   # Homebrew 자동 업데이트 방지
+cd ios && pod install
